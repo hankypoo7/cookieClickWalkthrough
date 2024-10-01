86e2b23ca0aa83b4db28b2d85847c90e48cb72c9
Hankypoo7 <100436822+hankypoo7@users.noreply.github.com>
2024-10-01 13:28:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
+//declare variables
 let cookies = 0;
 let power = 0;
-let cost = power * 1.1;
+let cost = 10;
 
+//create functions
 function clicked() {
-  if (power = 0) {
-    cookies = cookies + 1;
-    document.getElementById("cookiesText").innerHTML = cookies;
-  }
-  else {
-    cookies = cookies * power;
-    document.getElementById("cookiesText").innerHTML = cookies;
+    if (power === 0) { 
+        cookies += 1;
+    } else {
+        cookies += power;  // Increase cookies based on current power
+    }
+    document.getElementById("cookiesText").innerHTML = cookies; // Update text showing # of cookies
 }
 
 function powerBuy() {
-  if (cookies > 9) {
-    if (cookies > cost) {
-      power = power + 1;
-      cookies = cookies - cost;
-      document.getElementById("cookiesText").innerHTML = cookies;
-      document.getElementById("powerText").innerHTML = power;
+    if (cookies >= cost) {  // Check if cookies are enough for the cost
+        power += 1; // Add 1 to power
+        cookies -= cost;  // Subtract cost from cookies
+        cost = Math.ceil(10 * Math.pow(1.1, power));  // Increase cost by 1.1 for every power upgrade
+        document.getElementById("cookiesText").innerHTML = cookies; // Update text showing # of cookies
+        document.getElementById("powerText").innerHTML = power; // Show amount of power
+        document.getElementById("costText").innerHTML = cost;  // Display new cost
+    } else {
+        console.log('Not enough cookies!'); // If cookies <= cost, log error message
     }
-    else {
-      console.log('Not enough cookies!')
-    }
-  else {
-    console.log('Not enough cookies!')
-  }
-    
+}
