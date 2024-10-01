45fabe913b7f0ea87c078b95ca14692e3eb74372
Hankypoo7 <100436822+hankypoo7@users.noreply.github.com>
2024-10-01 12:38:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 let cookies = 0;
+let power = 0;
+let cost = power * 1.1;
 
 function clicked() {
-  cookies = cookies + 1;
-  document.getElementById("cookiesText").innerHTML = cookies;
+  if (power = 0) {
+    cookies = cookies + 1;
+    document.getElementById("cookiesText").innerHTML = cookies;
+  }
+  else {
+    cookies = cookies * power;
+    document.getElementById("cookiesText").innerHTML = cookies;
 }
+
+function powerBuy() {
+  if (cookies > 9) {
+    if (cookies > cost) {
+      power = power + 1;
+      cookies = cookies - cost;
+      document.getElementById("cookiesText").innerHTML = cookies;
+      document.getElementById("powerText").innerHTML = power;
+    }
+    else {
+      console.log('Not enough cookies!')
+    }
+  else {
+    console.log('Not enough cookies!')
+  }
+    
